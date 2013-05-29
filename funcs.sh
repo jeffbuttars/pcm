cfe49c9a9459a72333def586062e8da94c82e556
Jeff Buttars <jeffbuttars@gmail.com>
2013-05-28 18:00:58 -0600

    Add a command passthrough for commands we don't know.

--- funcs.sh
+++ funcs.sh
@@ -29,21 +29,18 @@ pcm_sync_expire()
         echo "No expire time set"
         echo $now > "$PCM_LAST_SYNC"
         echo $(pcm_sync $@)
-        exit
     fi
 
     if [[ $PCM_SYNC_EXPIRE -eq 0 ]]; then
         echo "expire is 0, always sync"
         echo $now > "$PCM_LAST_SYNC"
         echo $(pcm_sync $@)
-        exit
     fi
 
     if [[ ! -f "$PCM_LAST_SYNC" ]]; then
         echo "No last sync found, syncing."
         echo $now > "$PCM_LAST_SYNC"
         echo $(pcm_sync $@)
-        exit
     fi
 
     # aged_date=$(date --date="@$(cat $PCM_LAST_SYNC)")
@@ -60,12 +57,10 @@ pcm_sync_expire()
         echo "Last sync was at $(date --date="@$($aged_date)"), re-syncing..."
         echo $now > "$PCM_LAST_SYNC"
         echo $(pcm_sync $@)
-        exit
     fi
     
     next_sync=$(date -d "+$(echo $PCM_SYNC_EXPIRE - $delta | bc) min")
-    echo "Last sync was at $(date --date="@$aged_date"), not syncing until
-    $next_sync"
+    echo "Last sync was at $(date --date="@$aged_date"), not syncing until $next_sync"
 } #pcm_sync_expire
 
 pcm_install()
